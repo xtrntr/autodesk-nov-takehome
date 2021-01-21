856d32bc08e52e13a1634a97323552606ad9ce9a
Elysia Ong <elysia.ong@autodesk.com>
2021-01-21 18:09:25 +0800

    Rename test file

--- src/test/java/com/beta/replyservice/RestServiceApplicationTest.java
+++ src/test/java/com/beta/replyservice/RestServiceApplicationTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class RestServiceApplicationTests {
+public class RestServiceApplicationTest {
 
 	@Test
 	public void contextLoads() {
