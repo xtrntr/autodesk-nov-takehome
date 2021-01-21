613808c94e1740973c60c65ff99ddf88361c2a70
William Tran <william.tran@autodesk.com>
2021-01-21 15:43:56 +0800

    Initial commit

---
+++ src/main/java/com/beta/replyservice/ReplyController.java
@@ -0,0 +1,19 @@
+package com.beta.replyservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class ReplyController {
+
+	@GetMapping("/reply")
+	public ReplyMessage replying() {
+		return new ReplyMessage("Message is empty");
+	}
+
+	@GetMapping("/reply/{message}")
+	public ReplyMessage replying(@PathVariable String message) {
+		return new ReplyMessage(message);
+	}
+}
